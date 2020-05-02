25348efe5768382c5ecae936f143793d0cdfe48d
Alejandro Merino <alejandro.merino@easypaymentgateway.com>
2020-05-03 00:28:17 +0200

    Arreglo bucle

--- galeria.js
+++ galeria.js
@@ -3,35 +3,31 @@ window.onload = inicio;
 var figuras = ['01', '02', '03', '04', '05', '06', '07', '08'];
 var miniaturas = 3;
 var imgVisor = figuras[0];
-var tope = figuras.length;
+var tope = figuras.length - 1;
+var contador = 0;
 
-function inicio() {
-  //mostrar imagen principal
-  document.getElementById("divimgvisor").innerHTML =
-    `<img src="img/${figuras[0]}.jpg" alt="Imagen ${figuras[0]}" class="img-fluid" id="imgvisor">`;
-  //mostrar imagenes de las 4 miniaturas
-  for (var i = 1; i <= 4; i++) {
-    document.getElementById("dmini01").innerHTML +=
-      `<a href="#"><img src="img/${figuras[i]}.jpg" alt="Imagen ${figuras[i]}" class="img-fluid mini"></a>`;
-  }
+function mostrarPrincipal(i) {
+    document.getElementById("divimgvisor").innerHTML =
+        `<img src="img/${figuras[i]}.jpg" alt="Imagen ${figuras[i]}" class="img-fluid" id="imgvisor">`;
 }
 
-var contador = 0;
+function inicio() {
+    //mostrar imagen principal
+    mostrarPrincipal(0);
+    //mostrar imagenes de las 4 miniaturas
+    for (var i = 1; i <= 4; i++) {
+        document.getElementById("dmini01").innerHTML +=
+            `<a href="#"><img src="img/${figuras[i]}.jpg" alt="Imagen ${figuras[i]}" class="img-fluid mini"></a>`;
+    }
+}
+
 //pasa a la siguiente img en el array
 function fotoDerecha() {
-  do {
-    contador++;
-    document.getElementById("divimgvisor").innerHTML =
-      `<img src="img/${figuras[contador]}.jpg" alt="Imagen ${figuras[contador]}" class="img-fluid" id="imgvisor">`;
-  }
-  while (contador >= tope);
+    contador = (contador == tope) ? 0 : ++contador;
+    mostrarPrincipal(contador);
 }
 //pasa a la img anterior en el array
 function fotoIzquierda() {
-  do {
-    contador--;
-    document.getElementById("divimgvisor").innerHTML =
-      `<img src="img/${figuras[contador]}.jpg" alt="Imagen ${figuras[contador]}" class="img-fluid" id="imgvisor">`;
-  }
-  while (contador > 0);
+    contador = (contador == 0) ? tope : --contador;
+    mostrarPrincipal(contador);
 }
